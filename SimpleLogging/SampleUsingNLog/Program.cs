518c5ad8ec97b1d338c1f5fb6b30568a97c8f89a
rlimbaga <rlimbaga@MM00905>
2018-09-13 16:03:04 +0800

    Initial Commit for a sample logger using NLog

---
+++ SimpleLogging/SampleUsingNLog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace SampleUsingNLog
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Sample log file created. See log files");
+
+      Logger logger = LogManager.GetCurrentClassLogger();
+
+      logger.Debug("This is a debug message");
+
+      Console.ReadLine();
+
+    }
+  }
+}
