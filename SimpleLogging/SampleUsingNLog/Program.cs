b8c49cefd06d5870bcbc928923ea674dc5d00130
rlimbaga <rlimbaga@MM00905>
2018-09-13 16:14:10 +0800

    Removed unused usings

--- SimpleLogging/SampleUsingNLog/Program.cs
+++ SimpleLogging/SampleUsingNLog/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NLog;
+﻿using NLog;
+using System;
 
 namespace SampleUsingNLog
 {
-  class Program
+  internal class Program
   {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
       Console.WriteLine("Sample log file created. See log files");
 
